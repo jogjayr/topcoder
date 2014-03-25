6a85893c64bee6022d13ef8fc2904657ddb2399d
Jayraj Jog <jayraj@bittorrent.com>
2014-03-25 09:29:20 -0700

    Solved!!

--- cljr/src/cljr/magicalstrdiv1.clj
+++ cljr/src/cljr/magicalstrdiv1.clj
@@ -19,11 +19,44 @@
       ;check that the first half of the string is all > and the second half is all <
       (and (apply = (concat (subs a-str 0 split-index) [(quote \>)])) (apply = (concat (subs a-str split-index) [(quote \<)]))))))
 
+(defn larger [& nums]
+  "Returns the largest out of nums"
+  (last (sort nums)))
+
+(defn smaller [& nums]
+  "Returns the smallest out of nums"
+  (first (sort nums)))
+
 (defn count-char-ocurrence [given-str find-char]
   "Find ocurrences of find-char in given-str"
   (count (apply (fn [& args] (filter #(= find-char (str %)) args)) given-str)))  
 
+(defn lt-count [given-str]
+  "Find number of \"<\" in given-str"
+  (count-char-ocurrence given-str "<"))
+
+(defn gt-count [given-str]
+  "Find number of \">\" in given-str"
+  (count-char-ocurrence given-str ">"))
+
+(defn magical-length-at-partition [given-str partition-index]
+  "Find the length of the magical string in given-str if given-str is partitioned at partition-index"
+  (let [lhs (subs given-str 0 (inc partition-index))
+        rhs (subs given-str (inc partition-index))
+        num-gt (gt-count lhs)
+        num-lt (lt-count rhs)]
+    ;the largest magical string that can be produced at partition-index
+    ;is twice the number of > or < on either side, whichever of the two 
+    ;are fewer
+    (* 2 (smaller num-gt num-lt))))
+
+
 (defn get-longest [given-str]
-  (if (magical? given-str)
-    (count given-str)
-    5))
+  (let [count-given-str (count given-str)]
+    (if (magical? given-str)
+      ;if the string is already magical, just return its length
+        count-given-str
+        ;else, we need to divide the string into two parts such that
+        ;there are equal numbers of > and < in both parts and find the
+        ;partition that gives us the largest magical string
+        (last (sort (map magical-length-at-partition (repeat count-given-str given-str) (range 0 count-given-str)))))))
--- cljr/test/cljr/magicalstrdiv1_test.clj
+++ cljr/test/cljr/magicalstrdiv1_test.clj
@@ -12,6 +12,20 @@
     (is (= 4 (count-char-ocurrence "<<><>><" "<")))
     (is (= 4 (count-char-ocurrence "<<><>><" "<")))))
 
+(deftest larger-check
+  (testing "some numbers"
+    (is (= 5 (larger 3 5)))
+    (is (= 7 (larger 7 4)))))
+
+(deftest magical-at-partition
+  (testing ">><><<>"
+    (let [test-str ">><><<>"]
+      (is (= 2 ( magical-length-at-partition test-str 0)))
+      (is (= 4 (magical-length-at-partition test-str 1)))
+      (is (= 4 (magical-length-at-partition test-str 2)))
+      (is (= 4 (magical-length-at-partition test-str 3)))
+      (is (= 2 (magical-length-at-partition test-str 4))))))
+
 (deftest problem-tests 
   (testing "<><><<>"
     (is (= (get-longest "<><><<>")  4)))
