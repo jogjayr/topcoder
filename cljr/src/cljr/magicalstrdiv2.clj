1b865bf3366c632ed970bdc917ff12f54859c030
Jayraj Jog <jayraj@bittorrent.com>
2014-03-25 13:40:30 -0700

    Solved!

--- cljr/src/cljr/magicalstrdiv2.clj
+++ cljr/src/cljr/magicalstrdiv2.clj
@@ -1,4 +1,9 @@
-(ns cljr.magicalstrdiv2)
+(ns cljr.magicalstrdiv2
+  (:require cljr.magicalstrdiv1))
 
 (defn minimal-moves [given-str]
-  5)
+  (let [count-given-str (count given-str)
+        halfway (/ count-given-str 2)
+        lhs-str (subs given-str 0 halfway)
+        rhs-str (subs given-str halfway)]
+    (+ (cljr.magicalstrdiv1/lt-count lhs-str) (cljr.magicalstrdiv1/gt-count rhs-str))))
